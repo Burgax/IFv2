bd8283d63b62c9fcfd1e3bee4e59d1bc4d28d0c4
Burgax <99366877+Burgax@users.noreply.github.com>
2022-09-19 01:24:01 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+/*  -----------------------------------------------------------------------------------------------
+  Menu toggle
+--------------------------------------------------------------------------------------------------- */
+let item = document.querySelector('.menu-toggle');
+let menu = document.querySelector('.menu');
+item.addEventListener("click", function () {
+    menu.classList.toggle('menu--open');
+});
+
+
+
+
+
